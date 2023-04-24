146d167f534911f41149e1f66196cfdbc807ef8d
Jakub <jakubglinka9@gmail.com>
2023-04-24 15:05:24 +0200

    9. Change public instance variables to private wherever it was possible

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,11 +2,11 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int STACK_SIZE = 12;
-    public static final int EMPTY = -1;
+    private static final int STACK_SIZE = 12;
+    private static final int EMPTY = -1;
     private final int[] numbers = new int[STACK_SIZE];
 
-    public int total = EMPTY;
+    private int total = EMPTY;
 
     public void countIn(int in) {
         if (!isFull()) numbers[++total] = in;
