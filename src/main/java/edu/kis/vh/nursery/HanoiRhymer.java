9e50220df734e8dec10f4ca30ed98184f189d2c1
Jakub <jakubglinka9@gmail.com>
2023-05-08 15:10:26 +0200

    17. Add documentation to HanoiRhymer

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,13 +1,24 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Resporesent a Rhymer of type Hanoi
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     private int totalRejected = 0;
 
+    /**
+     * Reports a number of rejected elements
+     * @return number of rejected elements
+     */
     public int reportRejected() {
         return totalRejected;
     }
 
+    /**
+     * Adds element to the stack
+     * @param in element to be added to the stack
+     */
     @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo()) totalRejected++;
